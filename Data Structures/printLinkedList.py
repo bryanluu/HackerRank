a7af64e98e9eefe60a5e5ab7e6948f97697d53ab
Bryan Luu <bryanluu30794@gmail.com>
2019-11-15 08:22:21 -0800

    Added entry for insertion of LinkedList.

--- Data Structures/printLinkedList.py
+++ Data Structures/printLinkedList.py
@@ -45,3 +45,12 @@ def printLinkedList(head):
 
 
 if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
