d1681e90bf65887bf6f9af3fae491da8d94b0c28
Bryan Luu <bryanluu30794@gmail.com>
2019-11-27 16:05:27 -0800

    Added Huffman encoding/decoding and tree generation in julia.

--- Data Structures/BinarySearchTree.jl
+++ Data Structures/BinarySearchTree.jl
@@ -1,10 +1,11 @@
-#!/Applications/Julia-1.2.app/Contents/Resources/julia/bin/julia
+#!/Applications/Julia-1.3.app/Contents/Resources/julia/bin/julia
+using DataStructures
 import Base.show
 
 mutable struct BSTNode{T}
     data::T
-    left::Union{BSTNode, Nothing}
-    right::Union{BSTNode, Nothing}
+    left::Union{BSTNode{T}, Nothing}
+    right::Union{BSTNode{T}, Nothing}
 end
 
 mutable struct BinarySearchTree{T}
@@ -26,37 +27,37 @@ function createBST(data)
     end
 end
 
-# pre-order traversal print of the tree
-function preorderPrint(root::Union{BSTNode, Nothing})
+# pre-order traversal of the tree
+function preorder(root::Union{BSTNode, Nothing}, f::Function)
     if root == nothing
         return
     end
 
-    print(root.data, " ")
-    preorderPrint(root.left)
-    preorderPrint(root.right)
+    f(root.data)
+    preorder(root.left, f)
+    preorder(root.right, f)
 end
 
-# post-order traversal print of the tree
-function postorderPrint(root::Union{BSTNode, Nothing})
+# post-order traversal of the tree
+function postorder(root::Union{BSTNode, Nothing}, f::Function)
     if root == nothing
         return
     end
 
-    postorderPrint(root.left)
-    postorderPrint(root.right)
-    print(root.data, " ")
+    postorder(root.left, f)
+    postorder(root.right, f)
+    f(root.data)
 end
 
-# in-order traversal print of the tree
-function inorderPrint(root::Union{BSTNode, Nothing})
+# in-order traversal of the tree
+function inorder(root::Union{BSTNode, Nothing}, f::Function)
     if root == nothing
         return
     end
 
-    inorderPrint(root.left)
-    print(root.data, " ")
-    inorderPrint(root.right)
+    inorder(root.left, f)
+    f(root.data)
+    inorder(root.right, f)
 end
 
 # return the height of the tree
@@ -70,8 +71,8 @@ function height(root::Union{BSTNode, Nothing})
     return 1 + max(l, r)
 end
 
-# print the Top View of the tree
-function printTopView(root::Union{BSTNode, Nothing})
+# perform function f on the Top View of the tree
+function toporder(root::Union{BSTNode, Nothing}, f::Function)
     if root == nothing
         return
     end
@@ -104,11 +105,13 @@ function printTopView(root::Union{BSTNode, Nothing})
     # list of the node's data in top view order
     ans = [(m[k][2].data) for k in sort([i for i=keys(m)])]
 
-    println(join(ans, " "))
+    for a = ans
+        f(a)
+    end
 end
 
-# level-order traversal print of the tree
-function levelorderPrint(root::Union{BSTNode, Nothing})
+# level-order traversal of the tree
+function levelorder(root::Union{BSTNode, Nothing}, f::Function)
     if root == nothing
         return
     end
@@ -121,7 +124,7 @@ function levelorderPrint(root::Union{BSTNode, Nothing})
     while(!isempty(q))
         node = popfirst!(q)
 
-        print(node.data, " ")
+        f(node.data)
 
         if(node.left != nothing)
             append!(q, (node.left,))
@@ -184,32 +187,151 @@ function getLCA(root::Union{BSTNode, Nothing}, v1, v2)
     end
 end
 
+# get node from tree that satisfies f(node) = true, returns nothing if not found in tree
+function getNode(root::Union{BSTNode, Nothing}, f::Function)
+    if root == nothing
+        return nothing
+    end
+
+    if f(root)
+        return root
+    else
+        l = getNode(root.left, f)
+        if (l != nothing)
+            return l
+        else
+            return getNode(root.right, f)
+        end
+    end
+end
+
+mutable struct HuffNode
+    data::Char
+    freq::Int
+    parent::Union{BSTNode{HuffNode}, Nothing}
+end
+
+# create a Huffman Tree based on Huffman encoding of the given string s
+function createHuffTree(s::String)
+    ω = Dict{Char, Int}() # Map of Character to Frequency
+    pq = PriorityQueue{BSTNode, Int}() # Priority Queue of Node to Frequencies
+
+    for c = s
+        ω[c] = get!(ω, c, 0) + 1 # increment counter of character by 1
+    end # generate freq dict
+
+    for k = collect(keys(ω))
+        node = BSTNode(HuffNode(k, ω[k], nothing), nothing, nothing)
+        pq[node] = ω[k]
+    end # create leaf nodes
+
+    # following Huffman encoding algorithm
+    while(length(pq) > 1)
+        n1 = dequeue!(pq)
+        n2 = dequeue!(pq)
+
+        # create parent node
+        node = BSTNode(HuffNode(Char(0), n1.data.freq+n2.data.freq, nothing), n1, n2)
+        enqueue!(pq, node, n1.data.freq+n2.data.freq)
+
+        # assign parents
+        n1.data.parent = node
+        n2.data.parent = node
+    end
+
+    # root of Huffman Tree
+    root = dequeue!(pq)
+
+    return root
+end
+
+# encode a given string s using the given Huffman tree
+function encodeHuff(root::Union{BSTNode{HuffNode}, Nothing}, s::String)
+    ans = ""
+
+    for c = s
+        node = getNode(root, x->x.data.data == c) # get leaf
+        if node != nothing
+            curr = node
+            bit = ""
+            while(curr != root)
+                if(curr == curr.data.parent.left) # if current node is on left branch
+                    bit = string("0", bit)
+                else
+                    bit = string("1", bit)
+                end
+                curr = curr.data.parent # move up tree towards root
+            end
+            ans = string(ans, bit)
+        end
+    end
+
+    return ans
+end
+
+# return a decoded Huffman-encoded string of 0 and 1 using given Huffman Tree
+function decodeHuff(root::Union{BSTNode{HuffNode}, Nothing}, s::String)
+    curr = root
+    ans = ""
+
+    for c = s
+        if(c == '0')
+            curr = curr.left # move left
+        elseif(c == '1')
+            curr = curr.right # move right
+        end
+
+        if(curr.left == nothing && curr.right == nothing) # node is leaf
+            ans = string(ans, curr.data.data)
+            curr = root
+        end
+    end
+
+    return ans
+end
+
 # Main Function
 if abspath(PROGRAM_FILE) == @__FILE__
-    N = parse(Int, readline(stdin))
-    A = parse.(Int, split(readline(stdin)))
-    v1, v2 = parse.(Int, split(readline(stdin)))
+    # N = parse(Int, readline(stdin))
+    # A = parse.(Int, split(readline(stdin)))
+    # v1, v2 = parse.(Int, split(readline(stdin)))
 
-    tree = createBST(A)
+    # tree = createBST(A)
 
-    print("Pre-order: ")
-    preorderPrint(tree.root)
-    println()
-    print("Post-order: ")
-    postorderPrint(tree.root)
-    println()
-    print("In-order: ")
-    inorderPrint(tree.root)
-    println()
-    print("Height: ")
-    println(height(tree.root))
-    print("Top-view: ")
-    printTopView(tree.root)
-    print("Level-order: ")
-    levelorderPrint(tree.root)
-    println()
-    print("LCA: ")
-    print(getLCA(tree.root, v1, v2).data, "\n")
+    # # print function lambda
+    # pf = x->print(x, " ")
+
+    # print("Pre-order: ")
+    # preorder(tree.root, pf)
+    # println()
+    # print("Post-order: ")
+    # postorder(tree.root, pf)
+    # println()
+    # print("In-order: ")
+    # inorder(tree.root, pf)
+    # println()
+    # print("Height: ")
+    # println(height(tree.root))
+    # print("Top-order: ")
+    # toporder(tree.root, pf)
+    # println()
+    # print("Level-order: ")
+    # levelorder(tree.root, pf)
+    # println()
+    # print("LCA: ")
+    # print(getLCA(tree.root, v1, v2).data, "\n")
+
+    s = readline(stdin)
+
+    # print function lambda
+    pf = x->print(x.data == '\0' ? "0" : x.data)
+
+    root = createHuffTree(s)
+    preorder(root, pf)
+    code = encodeHuff(root, s)
+    println("Code: ", code)
+    ans = decodeHuff(root, code)
+    println("Decoded: ", ans)
 
     println()
 end
